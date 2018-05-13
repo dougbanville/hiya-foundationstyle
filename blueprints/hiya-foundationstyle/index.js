290b383bec895851c343fab8505acabbb84ddfae
dougbanville <doug.banville@rte.ie>
2018-05-13 16:16:58 +0100

    -

--- blueprints/hiya-foundationstyle/index.js
+++ blueprints/hiya-foundationstyle/index.js
@@ -9,41 +9,15 @@ module.exports = {
     return this.addPackageToProject('foundation-sites', "^6.4.3");
   },
 
+  included(app) {
+    this._super.included.apply(this, arguments);
+    this.ui.writeLine('Including external files!');
+  },
+
   // locals(options) {
   //   // Return custom template variables here.
   //   return {
   //     foo: options.entity.options.foo
   //   };
   // }
-
-   afterInstall(options) {
-     // Perform extra work here.
-     var stylePath = path.join(process.cwd(), 'app', 'styles');
-     var foundationPath = path.resolve(require.resolve('foundation-sites'), '../../../scss/');
-     var settingsPath = path.join(foundationPath, 'settings', '_settings.scss');
-     var settingsFile = fs.readFileSync(settingsPath);
-     var settingsFilePath = path.join(stylePath, '_settings.scss');
-
-     fs.writeFileSync(settingsFilePath, settingsFile);
-
-     return this.addPackagesToProject([
-      { name: 'ember-cli-sass', target: '^6.0.0' },
-      { name: 'broccoli-clean-css', target: '~1.1.0' }
-    ]);
-   },
-
-
-  /**
-   A wrapper method to write a message with writeWarnLine or writeLine, depending on which version
-   of ember-cli is used
-
-   @param {string} msg Message to print
-   */
-  _writeInfoLine: function(msg) {
-    if (this.ui.writeWarnLine) {
-        this.ui.writeWarnLine(msg);
-    } else {
-        this.ui.writeLine(msg, 'INFO');
-    }
-}
 };
